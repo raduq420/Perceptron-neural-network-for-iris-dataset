f48b6d30dfbcacaaf00f99382da7273ababa21d1
Radu Stefan <stefan-vladut.radu@student.tuiasi.ro>
2025-01-15 17:14:59 +0200

    Adaugare interfata neurala

--- MLPwithEvolutionaryAlgorithm/MainForm.cs
+++ MLPwithEvolutionaryAlgorithm/MainForm.cs
@@ -52,7 +52,43 @@ namespace MLPwithEvolutionaryAlgorithm
 
         private void buttonTrain_Click(object sender, EventArgs e)
         {
-           
+            bool check = int.TryParse(textBoxSizePop.Text, out int popsize);
+            bool check2 = int.TryParse(textBoxMaxGen.Text, out int maxPop);
+            bool check3 = double.TryParse(textBoxIncrucisare.Text, out double crossRate);
+            bool check4 = double.TryParse(textBoxMutatie.Text, out double mutRate);
+            if (!check || !check2 || !check3 || !check4)
+                MessageBox.Show("Campurile nu pot fi citite ca numere sau au tipul gresit de date!");
+            else if(crossRate>1 ||mutRate>1)
+                MessageBox.Show("Ratele trebuie sa fie intre 0 și 1!");
+            else
+            {
+                try
+                {
+                    EvolutionaryAlgorithm ea = new EvolutionaryAlgorithm();
+                    Chromosome solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMw13.Text = solution.Fitness.ToString();
+                    solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMw23.Text = solution.Fitness.ToString();
+                    solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMw14.Text = solution.Fitness.ToString();
+                    solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMw24.Text = solution.Fitness.ToString();
+                    solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMt3.Text = solution.Fitness.ToString();
+                    solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMt4.Text = solution.Fitness.ToString();
+                    solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMw35.Text = solution.Fitness.ToString();
+                    solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMw45.Text = solution.Fitness.ToString();
+                    solution = ea.Solve(new Iris(), popsize, maxPop, crossRate, mutRate);
+                    textBoxMt5.Text = solution.Fitness.ToString();
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("Fisierul cu date de intrare lipseste!");
+                }
+            }
         }
 
         /// <summary>
@@ -80,7 +116,13 @@ namespace MLPwithEvolutionaryAlgorithm
         /// </summary>
         private double MLP(double x1, double x2)
         {
-            
+            // se calculeaza suma intrarilor ponderate pentru fiecare neuron, scadem pragul si aplicam functia de activare 
+            // neuronul 5 are ca intrari iesirile neuronilor 3 si 4
+            // iesirea neuronului 5 este iesirea retelei
+            double N3 = ActivationFunction(w13 * x1 + w23 * x2 - t3);
+            double N4 = ActivationFunction(w14 * x1 + w24 * x2 - t4);
+            double N5 = ActivationFunction(w35 * N3 + w45 * N4 - t5);
+            return N5;
         }
         /// <summary>
         /// Selecteaza functia de activare atunci cand utilizatorul o alege din combobox
@@ -99,7 +141,13 @@ namespace MLPwithEvolutionaryAlgorithm
         /// </summary>
         private void ComputeResults()
         {
-          
+            textBoxResults.Clear();
+            for (int x1 = 0; x1 <= 1; x1++)
+                for (int x2 = 0; x2 <= 1; x2++)
+                {
+                    double y = MLP(x1, x2);
+                    textBoxResults.AppendText(string.Format("{0}   {1}   {2:F3}\r\n", x1, x2, y));
+                }
         }
         /// <summary>
         /// Citeste valorile ponderilor si pragurilor din interfata grafica
@@ -125,14 +173,39 @@ namespace MLPwithEvolutionaryAlgorithm
             ReadParametersFromGUI();
             ComputeResults();
             pictureBoxRegions.Refresh();
-
         }
         /// <summary>
         /// Desenarea regiunilor de decizie pentru retea in picturebox. Intrarile retelei sunt aici numere reale din intervalul [-0.5, 1.5]
         /// </summary>
         private void pictureBoxRegions_Paint(object sender, PaintEventArgs e)
         {
+            int size = pictureBoxRegions.Width; // patrat
+            Bitmap b = new Bitmap(size, size);
+            Graphics g = Graphics.FromImage(b);
+            g.Clear(Color.White);
 
+            for (double x1 = -0.5; x1 < 1.5; x1 += 0.01)
+                for (double x2 = -0.5; x2 < 1.5; x2 += 0.01)
+                {
+                    double y = MLP(x1, x2);
+                    int gray = (int)(y * 255);
+                    Color c = Color.FromArgb(gray, gray, gray);
+
+                    int xScreen = (int)(size * (x1 + 0.5) / 2.0);
+                    int yScreen = size - (int)(size * (x2 + 0.5) / 2.0);
+
+                    g.DrawLine(new Pen(c), xScreen, yScreen, xScreen, yScreen + 1);
+                }
+
+            for (int x1 = 0; x1 <= 1; x1++)
+                for (int x2 = 0; x2 <= 1; x2++)
+                {
+                    int xScreen = (int)(size * (x1 + 0.5) / 2.0);
+                    int yScreen = size - (int)(size * (x2 + 0.5) / 2.0);
+                    g.DrawEllipse(Pens.Red, xScreen - 2, yScreen - 2, 4, 4);
+                }
+
+            e.Graphics.DrawImage(b, 0, 0);
         }
     }
 }
--- MLPwithEvolutionaryAlgorithm/Operations.cs
+++ MLPwithEvolutionaryAlgorithm/Operations.cs
@@ -8,7 +8,32 @@ namespace MLPwithEvolutionaryAlgorithm
     /// </summary>
     public static class Selection
     {
-      
+        private static Random _random = new Random();
+
+        public static Chromosome Tournament(Chromosome[] population)
+        {
+            int idx1 = _random.Next(population.Length);
+            int idx2;
+            do
+            {
+                idx2 = _random.Next(population.Length);
+            } while (idx1 == idx2);
+
+            return population[idx1].Fitness > population[idx2].Fitness ? population[idx1] : population[idx2];
+        }
+
+        public static Chromosome GetBest(Chromosome[] population)
+        {
+            Chromosome bestChromosome = population[0];
+            foreach (var chrom in population)
+            {
+                if (chrom.Fitness > bestChromosome.Fitness)
+                {
+                    bestChromosome = chrom;
+                }
+            }
+            return new Chromosome(bestChromosome);
+        }
     }
 
     //==================================================================================
@@ -18,9 +43,33 @@ namespace MLPwithEvolutionaryAlgorithm
     /// </summary>
     public static class Crossover
     {
+        private static Random _random = new Random();
+
+        public static Chromosome Arithmetic(Chromosome mother, Chromosome father, double rate)
+        {
+            if (_random.NextDouble() < rate)
+            {
+                double coefficient = _random.NextDouble();
+                double[] offspringGenes = new double[father.NoGenes];
+                for (int i = 0; i < father.NoGenes; i++)
+                {
+                    //ne folosim de incrucisarea reala
+
+                    offspringGenes[i] = coefficient * mother.Genes[i] + (1 - coefficient) * father.Genes[i];
+
+                }
+                Chromosome offspring = new Chromosome(father)
+                {
+                    Genes = offspringGenes
+                };
+                return offspring;
+            }
+
+            return _random.Next(2) == 0 ? father : mother;
+        }
+    }
 
     //==================================================================================
-
 
     /// <summary>
     /// Clasa pentru operatia de mutatie
@@ -53,7 +102,46 @@ namespace MLPwithEvolutionaryAlgorithm
 
         public Chromosome Solve(IOptimizationProblem problem, int populationSize, int maxGenerations, double crossoverRate, double mutationRate)
         {
-            
+            try
+            {
+                _dataLines = File.ReadAllLines("iris.data");
+
+                Chromosome[] population = new Chromosome[populationSize];
+                InitializePopulation(population, problem);
+
+                for (int generation = 0; generation < maxGenerations; generation++)
+                {
+                    Chromosome[] newPopulation = new Chromosome[populationSize];
+                    newPopulation[0] = Selection.GetBest(population); // Elitism
+
+                    for (int i = 1; i < populationSize; i++)
+                    {
+                        //luam 2 parinti prin tournament
+                        Chromosome parent1 = Selection.Tournament(population);
+                        Chromosome parent2 = Selection.Tournament(population);
+
+                        //generam copil folosindu-ne de crossover
+                        Chromosome child = Crossover.Arithmetic(parent1, parent2, crossoverRate);
+
+                        //aplicam si mutatia pe copil
+                        Mutation.ApplyMutation(child, mutationRate);
+
+                        //facem fitness pentru copil
+                        problem.ComputeFitness(child);
+
+                        //adaugam copilul in populatia noua
+                        newPopulation[i] = child;
+                    }
+
+                    population = newPopulation;
+                }
+
+                return Selection.GetBest(population);
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new Exception("Data file not found.", ex);
+            }
         }
 
         private void InitializePopulation(Chromosome[] population, IOptimizationProblem problem)
